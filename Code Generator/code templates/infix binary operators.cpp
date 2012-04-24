deb7f955e4567ff86a9d4a984c35868bca812d8e
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2012-04-24 03:30:19 +0300

    Added copyright notices and license.
    Also added some macro definitions to allow each configuration to specify whether
    it needs to link with debug or release libraries.

--- Code Generator/code templates/infix binary operators.cpp
+++ Code Generator/code templates/infix binary operators.cpp
@@ -1,3 +1,21 @@
+//	Copyright (C) 2008, 2012 Vaptistis Anogeianakis <el05208@mail.ntua.gr>
+/*
+ *	This file is part of Code Generator.
+ *
+ *	Code Generator is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	Code Generator is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with Code Generator.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "../Real Matrices and Vectors.h"
 using namespace ASL;
 
